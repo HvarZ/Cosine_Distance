80481cab11e2245f694dcaedd201d69a1f49fb35
HvarZ <hvarzahar@gmail.com>
2021-11-08 23:02:11 +0300

    add base struct as simple vector

--- single/include/cos_distance.h
+++ single/include/cos_distance.h
@@ -4,14 +4,26 @@
 #include <stdlib.h>
 #include <float.h>
 #include <math.h>
+#include <stdio.h>
 
 typedef struct vector {
   float* data;
   size_t size;
 } vector_t;
 
+typedef struct vectors {
+  vector_t* data;
+  size_t size;
+} base_t;
+
+void CreateBase(base_t* base, size_t numberLines,
+                   size_t numberElements);
+void ClearBase(base_t* base);
+
 float Dot(vector_t* vector1, vector_t* vector2);
 float Length(vector_t* vector);
 float CosDistance(vector_t* vector1, vector_t* vector2);
+
+void ReadBaseFromFile(base_t* base, char* filename);
 
 #endif // INDIVIDUAL_TASK_2_COS_DISTANCE_H
--- single/sources/cos_distance.c
+++ single/sources/cos_distance.c
@@ -1,5 +1,34 @@
 #include "cos_distance.h"
 
+void CreateBase(base_t* base, size_t numberLines,
+                   size_t numberElements) {
+  if (base == NULL) {
+    return;
+  }
+  base->data = malloc(numberLines * sizeof(vector_t));
+  base->size = numberLines;
+
+  for (size_t i = 0; i < numberLines; ++i) {
+    base->data[i].data = malloc(numberElements * sizeof(float));
+    base->data[i].size = numberElements;
+  }
+}
+
+void ClearBase(base_t* base) {
+  if (base == NULL || base->data == NULL) {
+    return;
+  }
+
+  if (base->size > 0) {
+    for (size_t i = 0; i < base->size; i++) {
+      if (base->data[i].data != NULL) {
+        free(base->data[i].data);
+      }
+    }
+    free(base->data);
+  }
+}
+
 float Dot(vector_t* vector1, vector_t* vector2) {
   if (vector1->data == NULL || vector2->data == NULL) {
     return -FLT_MAX;
@@ -31,4 +60,26 @@ float Length(vector_t* vector) {
 
 float CosDistance(vector_t* vector1, vector_t* vector2) {
   return Dot(vector1, vector2) / (Length(vector1) * Length(vector2));
+}
+
+void ReadBaseFromFile(base_t* base, char* filename) {
+  if (filename == NULL) {
+    printf("file not found");
+    return;
+  }
+
+  FILE* in = fopen(filename, "r");
+  size_t numberLines, numberElements;
+  fscanf(in, "%lu %lu", &numberLines, &numberElements);
+
+  ClearBase(base);
+
+  CreateBase(base, numberLines, numberElements);
+
+  for (size_t i = 0; i < numberLines; i++) {
+    for (size_t j = 0; j < numberElements; j++) {
+      fscanf(in, "%f", &base->data[i].data[j]);
+    }
+  }
+  fclose(in);
 }
