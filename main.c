a16675023b9e5dd6e26f84f3d4ee0ec569b07272
HvarZ <hvarzahar@gmail.com>
2021-11-09 00:23:31 +0300

    add first test single laboratory for test

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
 #include "cos_distance.h"
+#include "file_generator.h"
 
 int main() {
-    return 0;
+  RandomFillBaseFile("file.txt", 10, 10);
+
+  base_t base;
+  ReadBaseFromFile(&base, "file.txt");
+
+  vector_t vector;
+  FillVector(&vector);
+
+  const vector_t* nearestVector = MinVectorCosDistance(&base, &vector);
+
+  for (size_t i = 0; i < nearestVector->size; ++i) {
+    printf("%f ", nearestVector->data[i]);
+  }
+  printf("\n");
+
+
+  ClearBase(&base);
+  return 0;
 }
