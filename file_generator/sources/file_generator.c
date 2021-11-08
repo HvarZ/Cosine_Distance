602cbdbf91cd6a488dcbfb2bbaea06c284c114c7
HvarZ <hvarzahar@gmail.com>
2021-11-08 20:36:25 +0300

    add realization of file_generator (4 functions : 2 randoms and 2 users)

---
+++ file_generator/sources/file_generator.c
@@ -0,0 +1,71 @@
+#include "file_generator.h"
+
+void FillVectorFile(FILE* out, size_t numberElements) {
+  if (out == NULL) {
+    return;
+  }
+  if (numberElements <= 0) {
+    printf("elements is 0");
+    return;
+  }
+
+  float buffer;
+  for (size_t i = 0; i < numberElements; ++i) {
+    scanf("%f", &buffer);
+    fprintf(out, "%f ", buffer);
+  }
+  fprintf(out, "\n");
+
+}
+
+void FillVectorsFile(char *filename, size_t numberLines,
+                     size_t numberElements) {
+  if (filename == NULL) {
+    printf("file not found");
+    return;
+  }
+  if (numberLines == 0) {
+    printf("elements is 0");
+    return;
+  }
+
+  FILE* out = fopen(filename, "w");
+  for (size_t i = 0; i < numberLines; ++i) {
+    FillVectorFile(out, numberElements);
+  }
+  fclose(out);
+}
+
+void RandomFillVectorFile(FILE *out, size_t numberElements) {
+  if (out == NULL) {
+    return;
+  }
+  if (numberElements <= 0) {
+    printf("elements is 0");
+    return;
+  }
+
+  for (size_t i = 0; i < numberElements; ++i) {
+    fprintf(out, "%f ", (float)(rand() % 2000) - 1000);
+  }
+  fprintf(out, "\n");
+}
+
+void RandomFillVectorsFile(char *filename, size_t numberLines,
+                           size_t numberElements) {
+  if (filename == NULL) {
+    printf("file not found");
+    return;
+  }
+  if (numberLines == 0) {
+    printf("elements is 0");
+    return;
+  }
+  FILE* out = fopen(filename, "w");
+  for (size_t i = 0; i < numberLines; ++i) {
+    srand(time(NULL) + i);
+    RandomFillVectorFile(out, numberElements);
+  }
+
+  fclose(out);
+}
