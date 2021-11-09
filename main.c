193231d75f5956d4b4cb064c5793748b41cf9aa0
HvarZ <hvarzahar@gmail.com>
2021-11-09 20:05:16 +0300

    add getopt in main

--- main.c
+++ main.c
@@ -1,23 +1,92 @@
 #include "neutral_functions.h"
 #include "file_generator.h"
 
-int main() {
-  RandomFillBaseFile("file.txt", 10, 10);
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
+
+#define MAX_BASE_SIZE 1000000
+#define MAX_VECTOR_SIZE 3
+
+int main(int argc, char *argv[]) {
+  int opt;
+  char *opts = "i:o:gh";
+  char *input = NULL;
+  char *output = NULL;
+  vector_t vector;
+
+  while ((opt = getopt(argc, argv, opts)) != -1) {
+    switch (opt) {
+    case 'i':
+      input = malloc((strlen(optarg) + 1) * sizeof(char));
+      strcpy(input, optarg);
+      break;
+    case 'o':
+      output = malloc((strlen(optarg) + 1) * sizeof(char));
+      strcpy(output, optarg);
+      break;
+    case 'g':
+      if (input != NULL) {
+        RandomFillBaseFile(input, MAX_BASE_SIZE, MAX_VECTOR_SIZE);
+        RandomFillVector(&vector, MAX_VECTOR_SIZE);
+      }
+      break;
+    case 'h':
+      printf("Usage: %s [-h|-i|-o|-g] <args>\n", argv[0]);
+      break;
+    default:
+      break;
+    }
+  }
 
   base_t base;
-  ReadBaseFromFile(&base, "file.txt");
+  CreateBase(&base, MAX_BASE_SIZE, MAX_VECTOR_SIZE);
+  ReadBaseFromFile(&base, input);
 
-  vector_t vector;
-  FillVector(&vector);
+  struct timespec start, end;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
 
   const vector_t* nearestVector = MinVectorCosDistance(&base, &vector);
 
-  for (size_t i = 0; i < nearestVector->size; ++i) {
-    printf("%f ", nearestVector->data[i]);
-  }
-  printf("\n");
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  size_t time = (1000000000 * (end.tv_sec - start.tv_sec) +
+                 (end.tv_nsec - start.tv_nsec)) / 1000;
 
+  if (!output) {
+    printf("Result : ");
+    for (size_t i = 0; i < nearestVector->size; ++i) {
+      printf("%f ", nearestVector->data[i]);
+    }
+    printf("\n");
+    printf("Time : %zu mks\n\n", time);
+  } else {
+    FILE *file = fopen(output, "w");
+
+    if (!file) {
+      printf("Opening file failed\n");
+      return 1;
+    }
+
+    fprintf(file, "Result : ");
+    for (size_t i = 0; i < nearestVector->size; ++i) {
+      fprintf(file, "%f ", nearestVector->data[i]);
+    }
+    fprintf(file,"\n");
+    fprintf(file, "Time : %zu mks\n\n", time);
+
+    if (fclose(file)) {
+      printf("Closing file failed\n");
+      return 1;
+    }
+  }
 
   ClearBase(&base);
-  return 0;
+  ClearVector(&vector);
+
+
+  free(output);
+  free(input);
+
+  return EXIT_SUCCESS;
 }
